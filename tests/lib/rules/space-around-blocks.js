7226c77ab46ba6aa53cc1e53caaa579cb1532bfb
Michael P. Geraci <me@mgeraci.com>
2016-07-01 12:03:15 -0400

    grab either tokens or body

--- tests/lib/rules/space-around-blocks.js
+++ tests/lib/rules/space-around-blocks.js
@@ -6,11 +6,23 @@ var Errors = require("../../../lib/errors");
 
 var ruleTester = new RuleTester();
 
+var parserOptions = {
+  ecmaVersion: 6,
+  ecmaFeatures: {
+    jsx: true
+  },
+	sourceType: "module"
+};
+
 ruleTester.run("space-before-blocks", rule, {
 	valid: [
 		"\nvar a = 1;\n\nif (true) { console.log('whatever') } else { console.log('yeah') }\n\nvar b = 1;",
 		"\n// preceding comment is ok\nif (true) { console.log('whatever') } else { console.log('yeah') }\n\nvar b = 1;",
 		"\nvar a = 1;\n\nswitch(dog) { case 'pit bull': console.log('sweet dog, dawg'); break; }\n\nvar b = 1;",
+		{
+			code: 'const LocationZipOrCity = React.createClass({\n\trender() {\n\t\tconst placeholder = locationCountry === "US" ?\n\t\t\t_str("Zip code") :\n\t\t\t_str("City");\n\n\t\tconst a = 1;\n\n\t\tif (placeholder === a) {\n\t\t\tconsole.log("oh fuck");\n\t\t}\n\n\t\treturn (\n\t\t\t<div className="settings-field-input">\n\t\t\t\tyo\n\t\t\t</div>\n\t\t);\n\t},\n});\n\nexport default LocationZipOrCity;\n',
+			parserOptions: parserOptions
+		},
 	],
 
 	invalid: [
@@ -23,6 +35,14 @@ ruleTester.run("space-before-blocks", rule, {
 		},
 		{
 			code: "\nif (true) { console.log('whatever') }\nvar a = 1;",
+			errors: [{
+				message: Errors.blockBlank,
+				type: "IfStatement"
+			}]
+		},
+		{
+			code: 'const LocationZipOrCity = React.createClass({\n\trender() {\n\t\tconst placeholder = locationCountry === "US" ?\n\t\t\t_str("Zip code") :\n\t\t\t_str("City");\n\n\t\tconst a = 1;\n\t\tif (placeholder === a) {\n\t\t\tconsole.log("oh fuck");\n\t\t}\n\n\t\treturn (\n\t\t\t<div className="settings-field-input">\n\t\t\t\tyo\n\t\t\t</div>\n\t\t);\n\t},\n});\n\nexport default LocationZipOrCity;\n',
+			parserOptions: parserOptions,
 			errors: [{
 				message: Errors.blockBlank,
 				type: "IfStatement"
